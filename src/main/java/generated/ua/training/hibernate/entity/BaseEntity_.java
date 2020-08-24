e68b6ccef520d3407aa16c33ca5ee6d9f6ae0e8c
Artem <radvansky@ukr.net>
2020-08-24 14:14:29 +0300

    decomposition

---
+++ src/main/java/generated/ua/training/hibernate/entity/BaseEntity_.java
@@ -0,0 +1,15 @@
+package ua.training.hibernate.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(BaseEntity.class)
+public abstract class BaseEntity_ {
+
+	public static volatile SingularAttribute<BaseEntity, Long> id;
+	public static volatile SingularAttribute<BaseEntity, CommonFields> commonFields;
+
+}
+
---
+++ src/main/java/generated/ua/training/hibernate/entity/CommonFields_.java
@@ -0,0 +1,14 @@
+package ua.training.hibernate.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(CommonFields.class)
+public abstract class CommonFields_ {
+
+	public static volatile SingularAttribute<CommonFields, String> name;
+
+}
+
